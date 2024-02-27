1bcada9080e25b7ca943af020adbe8a61d1a21f2
Arthur <arthur.barrabonita@gmail.com>
2024-02-27 09:38:06 -0300

    27/02 - Nuget Gallery

--- Cozastore/Models/Categoria.cs
+++ Cozastore/Models/Categoria.cs
@@ -13,4 +13,17 @@ public class Categoria
     [Required(ErrorMessage = "Por favor, informe o Nome")]
     [StringLength(30, ErrorMessage = "O Nome deve possui no máximo 30 caracteres")]
     public string Nome { get; set; }
+
+    [StringLength(300)]
+    public string Foto { get; set; }
+
+    [Display(Name = "Exibir com filtro?")]
+    public bool Filtrar {get; set; }
+    
+    [Display(Name = "Exibir com Banner?")]
+    public bool Banner { get; set; }
+    [Display(Name = "Categoria Mãe")]
+    public int? CatergoriaMaeId { get; set; }
+    [ForeignKey("CategoriaMaeId")]
+    public Categoria CategoriaMae { get; set; }
 }
