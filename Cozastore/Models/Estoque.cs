df493538ea2d7aedf39be0591d065730deddd1eb
Arthur <arthur.barrabonita@gmail.com>
2024-03-05 09:35:12 -0300

    Finalização da camada de dedos, crição do contexto e serviço de conexão

--- Cozastore/Models/Estoque.cs
+++ Cozastore/Models/Estoque.cs
@@ -6,8 +6,6 @@ namespace Cozastore.Models;
 [Table("Estoque")]
 public class Estoque
 {
-    private Produto produto;
-
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id  { get; set; }
