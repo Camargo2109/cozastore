fa789c1ae30056e897b5eae7473cb5b2dbe9f743
Arthur <arthur.barrabonita@gmail.com>
2024-04-02 09:39:21 -0300

    admin page

--- Cozastore/Controllers/CategoriasController.cs
+++ Cozastore/Controllers/CategoriasController.cs
@@ -3,29 +3,28 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Cozastore.Data;
 using Cozastore.Models;
-using System.Net;
 
 namespace Cozastore.Controllers;
 
-public class CategoriaController : Controller
+public class CategoriasController : Controller
 {
     private readonly AppDbContext _context;
     private readonly IWebHostEnvironment _host;
 
-    public CategoriaController(AppDbContext context, IWebHostEnvironment host)
+    public CategoriasController(AppDbContext context, IWebHostEnvironment host)
     {
         _context = context;
         _host = host;
     }
 
-    // GET: Categoria
+    // GET: Categorias
     public async Task<IActionResult> Index()
     {
         var appDbContext = _context.Categorias.Include(c => c.CategoriaMae);
         return View(await appDbContext.ToListAsync());
     }
 
-    // GET: Categoria/Details/5
+    // GET: Categorias/Details/5
     public async Task<IActionResult> Details(int? id)
     {
         if (id == null)
@@ -44,31 +43,43 @@ public class CategoriaController : Controller
         return View(categoria);
     }
 
-    // GET: Categoria/Create
+    // GET: Categorias/Create
     public IActionResult Create()
     {
         ViewData["CategoriaMaeId"] = new SelectList(_context.Categorias, "Id", "Nome");
         return View();
     }
 
-    // POST: Categoria/Create
+    // POST: Categorias/Create
     // To protect from overposting attacks, enable the specific properties you want to bind to.
     // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,Nome,Foto,Filtrar,Banner,CategoriaMaeId")] Categoria categoria)
+    public async Task<IActionResult> Create([Bind("Id,Nome,Foto,Filtrar,Banner,CategoriaMaeId")] Categoria categoria, IFormFile Foto)
     {
         if (ModelState.IsValid)
         {
             _context.Add(categoria);
             await _context.SaveChangesAsync();
+            if (Foto != null)
+            {
+                string fileName = categoria.Id + Path.GetExtension(Foto.FileName);
+                string upload = Path.Combine(_host.WebRootPath, "img\\categorias");
+                string newFile = Path.Combine(upload, fileName);
+                using(var stream = new FileStream(newFile, FileMode.Create))
+                {
+                    Foto.CopyTo(stream);
+                }
+                categoria.Foto = "\\img\\categorias\\" + fileName;
+                await _context.SaveChangesAsync();
+            }
             return RedirectToAction(nameof(Index));
         }
         ViewData["CategoriaMaeId"] = new SelectList(_context.Categorias, "Id", "Nome", categoria.CategoriaMaeId);
         return View(categoria);
     }
 
-    // GET: Categoria/Edit/5
+    // GET: Categorias/Edit/5
     public async Task<IActionResult> Edit(int? id)
     {
         if (id == null)
@@ -85,12 +96,12 @@ public class CategoriaController : Controller
         return View(categoria);
     }
 
-    // POST: Categoria/Edit/5
+    // POST: Categorias/Edit/5
     // To protect from overposting attacks, enable the specific properties you want to bind to.
     // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Foto,Filtrar,Banner,CategoriaMaeId")] Categoria categoria, IFormFile Foto)
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Foto,Filtrar,Banner,CategoriaMaeId")] Categoria categoria)
     {
         if (id != categoria.Id)
         {
@@ -99,29 +110,29 @@ public class CategoriaController : Controller
 
         if (ModelState.IsValid)
         {
- 
-         _context.Update(categoria);
-         await _context.SaveChangesAsync();
-         if(Foto !=null)
-         {
-            string fileName = categoria.Id + Path.GetExtension(Foto.FileName);
-            string upload = Path.Combine(_host.WebRootPath, "img\\categorias");
-            string newFile = Path.Combine(upload, fileName);
-            using (var stream = new FileStream(newFile, FileMode.Create))
+            try
             {
-                Foto.CopyTo(stream);
+                _context.Update(categoria);
+                await _context.SaveChangesAsync();
             }
-            categoria.Foto = "\\img\\categorias\\" + fileName;
-            await _context.SaveChangesAsync();
-         }
-    
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriaExists(categoria.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
             return RedirectToAction(nameof(Index));
         }
         ViewData["CategoriaMaeId"] = new SelectList(_context.Categorias, "Id", "Nome", categoria.CategoriaMaeId);
         return View(categoria);
     }
 
-    // GET: Categoria/Delete/5
+    // GET: Categorias/Delete/5
     public async Task<IActionResult> Delete(int? id)
     {
         if (id == null)
@@ -140,7 +151,7 @@ public class CategoriaController : Controller
         return View(categoria);
     }
 
-    // POST: Categoria/Delete/5
+    // POST: Categorias/Delete/5
     [HttpPost, ActionName("Delete")]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> DeleteConfirmed(int id)
@@ -160,4 +171,3 @@ public class CategoriaController : Controller
         return _context.Categorias.Any(e => e.Id == id);
     }
 }
-
